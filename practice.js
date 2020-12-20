b23f6e84dc82b9958b334bb06fe5795308a86d2d
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-12-19 18:44:17 -0600

    Created best in show function that takes in string argument

--- practice.js
+++ practice.js
@@ -1,3 +1,19 @@
+//function for Best in Show practice
+const bestInShow = (dogBreed) => {
+    //if user passes "meow" into bestInShow, a response for cats will appear
+    if (dogBreed === "meow") {
+        return "I like cats"
+    }
+    const favoriteBreed = `My favorite dog breed is ${dogBreed}`
+    return favoriteBreed
+}
+
+const myFavorite = bestInShow("schnauzer");
+
+console.log(myFavorite)
+
+
+//function for calculating dog years
 const calcAgeInDogYear = (currentAge) => {
     const dogAge = currentAge / 7
     return dogAge
@@ -5,4 +21,4 @@ const calcAgeInDogYear = (currentAge) => {
 
 const ageInDogYears = calcAgeInDogYear(30)
 
-console.log(ageInDogYears)
+console.log(`Dog Year calculation${ageInDogYears}`)
