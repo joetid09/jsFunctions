554ee2d98871989fac2e78fda255d838691d00b3
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-12-19 19:00:45 -0600

    created function for self driving cars

--- practice.js
+++ practice.js
@@ -1,10 +1,29 @@
+
+
+//function for self driving car function
+const go = (direction, speedInMph) => {
+    const drivingMessage = `the car is moving ${direction} at ${speedInMph} mph`
+    if (speedInMph >= 75) {
+        return drivingMessage + ", Slow Down!"
+    }
+    else {
+        return drivingMessage
+    }
+}
+
+const drivingMessage = go("forward", 74)
+console.log("Self-Driving Cars : ", drivingMessage)
+
+
+//simple sum of parameters function
 const add = (num1, num2, num3) => {
     return num1 + num2 + num3
 }
 
 const sum = add(17, 4, 11)
 
-console.log("Result for Addition", sum)
+console.log("Result for Addition : ", sum)
+
 
 //function for Best in Show practice
 const bestInShow = (dogBreed) => {
@@ -18,7 +37,7 @@ const bestInShow = (dogBreed) => {
 
 const myFavorite = bestInShow("schnauzer");
 
-console.log("Result for bestInShow", myFavorite)
+console.log("Result for bestInShow : ", myFavorite)
 
 
 //function for calculating dog years
@@ -29,4 +48,4 @@ const calcAgeInDogYear = (currentAge) => {
 
 const ageInDogYears = calcAgeInDogYear(30)
 
-console.log(`Dog Year calculation${ageInDogYears}`)
+console.log(`Dog Year calculation : ${ageInDogYears}`)
